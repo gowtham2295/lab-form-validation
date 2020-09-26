e8b6876583013def58fffe830307a8ef24ad0b46
Sambhoju Gowtham <46318869+gowtham2295@users.noreply.github.com>
2020-09-26 18:11:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,30 @@
 // username should not contain special characters
 // the function should return true if all the conditions above are validated.
 // return false if the validation fails
+document.getElementById('submit').addEventListener('click', checkValidate);
+
+function checkValidate() {
+    checkUserName();
+    checkEmail();
+    checkPassword();
+}
+
+function checkUserName() {
+    var username = document.getElementById('usertext').value;
+    var specialChar = /[+\-=\[\]{};':"\\|,.<>\/?!@#$%^&*()_]+/; 
+    if (usertext == null || username.length < 8 || username.length > 15
+        || username.split('').includes(' ')) {
+        alert('Please enter valid Username!')
+        return false;
+    }
+    else if (specialChar.test(username)) {
+        alert('Please enter valid Username!')
+        return false;
+    }
+    else {
+        return true;
+    }
+}
 
 // Progression 2 
 // Check mail
@@ -16,9 +40,37 @@
 // It can be alphanumeric in nature
 // return true if it passses all validation and false otherwise
 
+function checkEmail() {
+    let mailId = document.getElementById('emailtext').value;
+    var specialChar = /[+\-=\[\]{};':"\\|,<>\/?!#$%^&*()]+/; //special characters 
+    if (mailId == null) {
+        alert('Please enter valid MailID!')
+        return false;
+    }
+    else if (specialChar.test(mailId)) {
+        alert('Please enter valid MailID!')
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
 // Progression 3
 // Check password
 // create a function called as checkPassword()
 // should be alphanumeric in nature
 // password must contain atleast one Uppercase, one number and special characters[!@#$%^&*()_]
 // return true else return false
+
+function checkPassword() {
+    var password = document.getElementById('passwordtext').value;
+    if (password.match(/[a-z]/g) && password.match(/[A-Z]/g) && password.match(/[0-9]/g)
+        && password.match(/[^a-zA-Z\d]/g)) {
+        return true;
+    }
+    else {
+        alert('Please enter valid password!');
+        return true;
+    }
+}
